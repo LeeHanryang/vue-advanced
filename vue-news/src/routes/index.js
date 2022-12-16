1959eb6fac2921a1acd3b1a303a68bc18dd152ee
이준혁 <ballack02@naver.com>
2022-12-16 16:55:02 +0900

    라우터 기본

---
+++ vue-news/src/routes/index.js
@@ -0,0 +1,39 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import NewsView from "../views/NewsView";
+import AskView from "../views/AskView";
+import JobsView from "../views/JobsView";
+import UserView from "../views/UserView";
+import ItemView from "../views/ItemView";
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      redirect: '/news',
+    },
+    {
+        path: '/news',   // url 주소
+        component: NewsView, // url 주소로 갔을 때 표시 될 컴포넌트
+    },
+    {
+        path: '/ask',
+        component: AskView,
+    },
+    {
+        path: '/jobs',
+        component: JobsView,
+    },
+    {
+      path: '/user',
+      component: UserView,
+    },
+    {
+      path: '/item',
+      component: ItemView,
+    },
+  ]
+});
