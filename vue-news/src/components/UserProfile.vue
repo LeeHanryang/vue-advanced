4e27051c9fe8daf9846986ef45faaa2464c8666c
이준혁 <ballack02@naver.com>
2022-12-21 09:01:49 +0900

    데이터 흐름 처리 1

---
+++ vue-news/src/components/UserProfile.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="user-container">
+        <div>
+            <i class="fa-solid fa-user"></i>
+        </div>
+        <div class="user-description">
+            <div>
+                {{ UserInfo.id }}
+            </div>
+            <!-- <router-link :to="`/user/${UserInfo.user}`">
+                {{ UserInfo.id }}
+            </router-link> -->
+            <div class="timeline">
+                {{ UserInfo.created }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+    computed: {
+      ...mapGetters({
+        UserInfo: 'fetchedUser',
+      }),
+    },
+}
+</script>
+
+<style scoped>
+  .user-container {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    padding: 0.5rem;
+  }
+  .fa-user {
+    font-size: 2.5rem;
+  }
+  .user-description {
+    padding: 8px;
+  }
+  .timeline {
+    font-size: 0.7rem;
+  }
+</style>
--- vue-news/src/views/UserView.vue
+++ vue-news/src/views/UserView.vue
@@ -1,20 +1,25 @@
 <template>
   <div>
-    <p>name : {{ userInfo.id }}</p>
+    <user-profile></user-profile>
+    <!-- <p>name : {{ userInfo.id }}</p>
     <p>karma : {{ userInfo.karma }}</p>
-    <p>created : {{ userInfo.created }}</p>
+    <p>created : {{ userInfo.created }}</p> -->
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
 // import axios from 'axios';
+import UserProfile from "../components/UserProfile.vue";
 export default {
-  computed: {
-    ...mapGetters({
-      userInfo: 'fetchedUser',
-    }),
+  components: {
+    UserProfile,
   },
+  // computed: {
+  //   ...mapGetters({
+  //     userInfo: 'fetchedUser',
+  //   }),
+  // },
   created() {
     const userName = this.$route.params.id;
     // axios.get(`https://api.hnpwa.com/v0/user/${userName}.json`);
