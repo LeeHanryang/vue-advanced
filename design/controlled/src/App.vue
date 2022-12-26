83df0467a32f4e1697527037b49eaea2d14e7d03
이준혁 <ballack02@naver.com>
2022-12-26 16:29:47 +0900

    design 폴더 run serve 오류

---
+++ design/controlled/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <check-box v-model="checked"></check-box>
+</template>
+
+<script>
+import CheckBox from './components/CheckBox.vue';
+
+export default {
+  data() {
+    return {
+      checked: false,
+    }
+  },
+  components: {
+    CheckBox
+  },
+}
+</script>
